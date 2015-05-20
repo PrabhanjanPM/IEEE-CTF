716cffce44b3f864afdeb437d5a39a854f7be4aa
PrabhanjanPM <prmannari@gmail.com>
2015-05-20 16:23:55 +0530

    -a

---
+++ Crypto/Session1/XORingStrings.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+        char a[100];
+        char b[100];
+        printf("Enter Plaintext- ");
+        gets(a);
+        printf("Enter key(same length as the plaintext)- ");
+        gets(b);
+        for(i=0;i<strlen(a);i++){
+                a[i]=a[i]^b[i];
+        }
+        printf("Ciphertext- %s\n",a);
+        return 0;
+}
